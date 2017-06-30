8fac47bdb592cf4bb1d2ab41b7c0836663ac7022
Syed Abdullah <contact@syedabdullah.net>
2017-07-01 01:24:20 +0800

    app: add basic structure for flask+SQLAlchemy

---
+++ mailroom/config.py
@@ -0,0 +1,7 @@
+'''
+Default configurations
+'''
+
+# Defines the URI of the database
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ mailroom/mailroom.py
@@ -0,0 +1,15 @@
+'''
+Entrypoint of the application
+'''
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Load appropriate objects
+app = Flask(__name__, instance_relative_config=True)
+
+# Load defaults first and override with instance
+app.config.from_object('mailroom.config')
+app.config.from_json('mailroom.cfg', silent=True)
+
+# Load database
+db = SQLAlchemy(app)
