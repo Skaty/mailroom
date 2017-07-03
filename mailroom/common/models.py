09af3d24e6259cc0eb72b7109b086b73ca96a796
Syed Abdullah <contact@syedabdullah.net>
2017-07-04 00:12:33 +0800

    models: improve + generalize serializer for models

--- mailroom/common/models.py
+++ mailroom/common/models.py
@@ -3,3 +3,8 @@ from ..mailroom import db
 class Base(db.Model):
     __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
+
+    def as_dict(self, keys = None):
+        if keys is None:
+            keys = [c.name for c in self.__table__.columns]
+        return {c: getattr(self, c) for c in keys if c in self.__table__.columns}
--- mailroom/mod_api/models.py
+++ mailroom/mod_api/models.py
@@ -5,6 +5,3 @@ class Mailbox(Base):
     name = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(256), nullable=False, unique=True)
     is_visible = db.Boolean()
-
-    def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
