9c47bb52a532fa5cc8bfdf8b6b98b14a7e84b7cc
Syed Abdullah <contact@syedabdullah.net>
2017-07-04 00:19:35 +0800

    api/ctrl: add loading of keys to controller

--- mailroom/config.py
+++ mailroom/config.py
@@ -7,3 +7,7 @@ import os
 # Defines the URI of the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.instance_path, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+KEYS = {
+    'recaptcha': ''
+}
--- mailroom/mod_api/controllers.py
+++ mailroom/mod_api/controllers.py
@@ -2,7 +2,15 @@ from flask import Blueprint, request, jsonify
 
 from .models import Mailbox
 
+keys = {}
+
 mod_api = Blueprint('main', __name__, url_prefix='/api')
+
+@mod_api.record_once
+def record_keys(setup_state):
+    global keys
+    app = setup_state.app
+    keys = app.config.get('KEYS', {})
 
 @mod_api.route('/mailboxes', methods=['GET'])
 def list_mailboxes():
