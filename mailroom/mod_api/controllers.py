eecb255e786ec38119892f5c6e18e29a3c6b1b7d
Syed Abdullah <contact@syedabdullah.net>
2017-07-04 00:18:37 +0800

    api/ctrl: add mailbox listing endpoint

--- mailroom/mod_api/controllers.py
+++ mailroom/mod_api/controllers.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 from .models import Mailbox
 
 mod_api = Blueprint('main', __name__, url_prefix='/api')
+
+@mod_api.route('/mailboxes', methods=['GET'])
+def list_mailboxes():
+    return_fields = ['id', 'name']
+    response = [x.as_dict(return_fields) for x in Mailbox.query.all()]
+    return jsonify(response)
 
 @mod_api.route('/send', methods=['GET','POST'])
 def send_message():
