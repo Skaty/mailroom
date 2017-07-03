bd748ca153e0f566c321a25e1a595153351f32ec
Syed Abdullah <contact@syedabdullah.net>
2017-07-03 23:48:24 +0800

    model: add columns + serializer in Mailbox

--- mailroom/mod_api/models.py
+++ mailroom/mod_api/models.py
@@ -2,4 +2,9 @@ from ..mailroom import db
 from ..common.models import Base
 
 class Mailbox(Base):
+    name = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(256), nullable=False, unique=True)
+    is_visible = db.Boolean()
+
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
