e0a9704d921ce47954533e190946b2632801f657
Syed Abdullah <contact@syedabdullah.net>
2017-07-04 21:24:12 +0800

    model: add support for mailgroups

--- mailroom/mod_api/models.py
+++ mailroom/mod_api/models.py
@@ -1,7 +1,12 @@
 from ..mailroom import db
 from ..common.models import Base
 
+class Mailgroup(Base):
+    name = db.Column(db.String(256), nullable=False)
+    mailboxes = db.relationship('Mailbox', backref='mailgroup', lazy='dynamic')
+
 class Mailbox(Base):
+    mailgroup_id = db.Column(db.Integer, db.ForeignKey('mailgroup.id'))
     name = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(256), nullable=False, unique=True)
     is_visible = db.Boolean()
