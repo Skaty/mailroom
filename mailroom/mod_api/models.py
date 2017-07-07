b319af7c18857b26705551c04f9f5c445ef8625c
Syed Abdullah <contact@syedabdullah.net>
2017-07-07 22:30:04 +0800

    api/model: fix error in Mailbox columns

--- mailroom/mod_api/models.py
+++ mailroom/mod_api/models.py
@@ -4,7 +4,7 @@ from ..common.models import Base
 class Mailgroup(Base):
     name = db.Column(db.String(256), nullable=False)
     mailboxes = db.relationship('Mailbox', backref='mailgroup', lazy='dynamic')
-    
+
     @property
     def serialize(self):
         serialized = super().serialize
@@ -15,8 +15,8 @@ class Mailbox(Base):
     mailgroup_id = db.Column(db.Integer, db.ForeignKey('mailgroup.id'))
     name = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(256), nullable=False, unique=True)
-    is_visible = db.Boolean()
-    
+    is_visible = db.Column(db.Boolean())
+
     @property
     def serialize(self):
         return self.serialize_some(['id', 'name'])
