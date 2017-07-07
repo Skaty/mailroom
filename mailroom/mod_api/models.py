9dcfa8973cad9c99fe3b8601e1693968af2b1064
Syed Abdullah <contact@syedabdullah.net>
2017-07-08 00:20:40 +0800

    model: add template column and ensure that mailbox serialization is done over visible mailboxes only

--- mailroom/mod_api/models.py
+++ mailroom/mod_api/models.py
@@ -8,13 +8,14 @@ class Mailgroup(Base):
     @property
     def serialize(self):
         serialized = super().serialize
-        serialized['mailboxes'] = [mailbox.serialize for mailbox in self.mailboxes.all()]
+        serialized['mailboxes'] = [mailbox.serialize for mailbox in self.mailboxes.all() if mailbox.is_visible]
         return serialized
 
 class Mailbox(Base):
     mailgroup_id = db.Column(db.Integer, db.ForeignKey('mailgroup.id'))
     name = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(256), nullable=False, unique=True)
+    template = db.Column(db.Text(), nullable=False)
     is_visible = db.Column(db.Boolean())
 
     @property
